fffcfc85d8ecf32b44b2d4fec0421e64c94d4023
Jordan Boggs <boggsjp@gmail.com>
2017-11-11 21:39:37 -0700

    built the bones for the html site

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+// Scoreboard
+var playerWins = 0;
+var playerLosses = 0;
+
+// Word bank for the RNG to pick from
+var wordBank = ['garnet',
+                'amethyst',
+                'pearl',
+                'steven',
+                'greg'];
+
+// number of guesses remaining for player
+var guessesLeft = 12;
+
+// Array of letters that player has already guessed
+var wrongLetters = [];
